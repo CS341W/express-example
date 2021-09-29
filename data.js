334e57425db568362dc485ca6bae05eeb7a491bd
CS341W-Admin <88976618+CS341W-Admin@users.noreply.github.com>
2021-09-29 14:43:02 -0700

    removes sample data

--- data.js
+++ data.js
@@ -1,19 +1,3 @@
-const movies = [
-  {
-    title: 'Star Wars',
-    year: '1977',
-    director: 'George Lucas',
-  },
-  {
-    title: 'Jurassic Park',
-    year: '1993',
-    director: 'Steven Spielberg',
-  },
-  {
-    title: 'Avatar',
-    year: '2009',
-    director: 'James Cameron',
-  },
-]
+const  = 
 
-module.exports = { movies }
+module.exports = 
